06cd067cd7c757e43928aff14206bd5f49c3757c
fajaralfath <fajaralfathrachmat@gmail.com>
2019-06-17 22:41:18 +0800

    perbaikan template baru

--- resources/views/partials/_footer.blade.php
+++
@@ -1,9 +0,0 @@
-
-        <!-- footer content -->
-        <footer>
-          <div class="pull-right">
-             Aplikasi Pemesanan Tiket Bioskop Secara Online <a href="#">FajarAlfath</a>
-          </div>
-          <div class="clearfix"></div>
-        </footer>
-              <!-- /footer content -->
